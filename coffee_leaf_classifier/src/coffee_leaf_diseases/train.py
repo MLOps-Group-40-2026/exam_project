7594460944ae925896cdd8e9a31dcaaedb98bb90
Seisision <ulrikhlp@gmail.com>
2026-01-09 12:24:16 +0100

    started project structure with cookiecutter template

---
+++ coffee_leaf_classifier/src/coffee_leaf_diseases/train.py
@@ -0,0 +1,10 @@
+from coffee_leaf_diseases.model import Model
+from coffee_leaf_diseases.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
