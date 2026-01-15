7c01b28d80e4108990691a20e8c4f66f848c2be3
Seisision <ulrikhlp@gmail.com>
2026-01-15 11:03:58 +0100

    Add FastAPI mock endpoints

--- src/coffee_leaf_classifier/test_wandb.py
+++ src/coffee_leaf_classifier/test_wandb.py
@@ -5,16 +5,16 @@ def test_connection():
 
     run = wandb.init(
         project="coffee_leaf_diseases",
-        entity="mlops-group-40", 
+        entity="mlops-group-40",
         name="initial-connection-test"
     )
-    
+
     acc = 0.1 + random.random() * 0.8
-    
+
     run.log({"test_accuracy": acc})
-    
+
     print(f"Successfully logged to: {run.url}")
-    
+
     run.finish()
 
 if __name__ == "__main__":
