7a403e7b457ab72e4d15ea4a5aef96992aced1b6
Seisision <ulrikhlp@gmail.com>
2026-01-15 12:16:46 +0100

    Add loguru logging to API

--- src/coffee_leaf_classifier/train.py
+++ src/coffee_leaf_classifier/train.py
@@ -1,12 +1,18 @@
+from loguru import logger
+
 from coffee_leaf_classifier.data import MyDataset
 from coffee_leaf_classifier.model import Model
 
 
 def train():
+    """Train the coffee leaf classifier model."""
+    logger.info("Starting training...")
     dataset = MyDataset("data/raw")  # noqa
     model = Model()  # noqa
     # TODO: add rest of your training code here
+    logger.info("Training complete")
 
 
 if __name__ == "__main__":
+    logger.info("Running training script")
     train()
