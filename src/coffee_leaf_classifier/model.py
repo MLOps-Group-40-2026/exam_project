f49c69ff1ed3f0cf116c669fd84d6b01763fabaf
YarinB <yarin10121@gmail.com>
2026-01-16 15:24:14 +0200

    Update model.py with dummy model for testing

--- src/coffee_leaf_classifier/model.py
+++ src/coffee_leaf_classifier/model.py
@@ -5,9 +5,11 @@ class Model(nn.Module):
     """Just a dummy model to show how to structure your code"""
     def __init__(self):
         super().__init__()
-        self.layer = nn.Linear(1, 1)
+        self.layer = nn.Linear(2048 * 1024 * 3, 5)
+        self.num_classes = 5
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.flatten(x, start_dim=1)
         return self.layer(x)
 
 if __name__ == "__main__":
